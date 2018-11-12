752dfe1396a8eddcd50a06553cc94d432673561b
ericrlin <eric@barnabasrobotics.com>
2018-11-12 10:41:39 -0800

    Add files via upload

---
+++ moving_motors_simultaineously.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo servo_1;
+Servo servo_2;
+Servo servo_3;
+
+void setup()
+{
+  servo_1.attach(9);
+  servo_2.attach(10);
+  servo_3.attach(11);
+}
+
+void loop()
+{
+  servo_1.write( 0 );
+  servo_2.write( 0 );
+  servo_3.write( 0 );
+  delay( 1000 );
+  servo_1.write( 180 );
+  servo_2.write( 180 );
+  servo_3.write( 180 );
+  delay( 1000 );
+}
+
+
---
+++ moving_one_motor.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo servo_1;
+
+void setup()
+{
+  servo_1.attach(9);
+}
+
+void loop()
+{
+  servo_1.write( 0 );
+  delay( 1000 );
+  servo_1.write( 180 );
+  delay( 1000 );
+}
+
+
