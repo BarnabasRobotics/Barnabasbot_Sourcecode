36a05f1d92dc6f6a1651dcbf95a707552311216a
ericrlin <eric@barnabasrobotics.com>
2018-12-12 15:36:57 -0800

    Add files via upload

---
+++ fastrepeatslowrepeat.ino
@@ -0,0 +1,24 @@
+void setup()
+{
+  pinMode( 7 , OUTPUT);
+}
+
+void loop()
+{
+  for (int i=0;i<5;i++)
+  {
+    digitalWrite( 7 , HIGH );
+    delay( 1000 );
+    digitalWrite( 7 , LOW );
+    delay( 1000 );
+  }
+  for (int i=0;i<5;i++)
+  {
+    digitalWrite( 7 , HIGH );
+    delay( 200 );
+    digitalWrite( 7 , LOW );
+    delay( 200 );
+  }
+}
+
+
